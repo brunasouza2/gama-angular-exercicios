cab9908173bb2a110572872d42dc98fa48f9f240
brunasouza2 <50778590+brunasouza2@users.noreply.github.com>
2019-07-02 13:48:35 -0300

    Exercicio 1
    Signed-off-by: brunasouza2 <50778590+brunasouza2@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'gama-angular-exercicios';
+export class AppComponent implements OnInit {
+  title = 'Exercício 1';
+  contador = 0;
+
+  ngOnInit(){
+
+  }
+
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
 }
